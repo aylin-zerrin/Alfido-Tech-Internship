fe3c6139bc7f2775d06756d3d46e1e904e86a565
aylin-zerrin <aylinzerrin2006@gmail.com>
2025-07-07 11:43:18 +0530

    Final Task 1: Responsive profile card with JS features
    - Added circular profile image and centered profile card layout
    - Included 'Greet Me' and 'Toggle Theme' buttons using JavaScript
    - Responsive for all devices with mobile-friendly design

---
+++ Task-1_Simple-Website/script.js
@@ -0,0 +1,9 @@
+// Show greeting popup
+function showGreeting() {
+  alert("Hi Aylin! Keep glowing and coding! 💖💻");
+}
+
+// Toggle dark/light mode
+function toggleTheme() {
+  document.body.classList.toggle("dark-mode");
+}
